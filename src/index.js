859636638e5341bf5f7d90318d987e126ad127e1
Leo Saglia <leosaglia@hotmail.com>
2021-04-17 17:36:19 -0300

    Desafio 2 resolvido - trabalhando com middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,76 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user)
+  {
+    return response.status(404).json({  error: "User does not exists" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const registeredTodos = user.todos.length;
+
+  if (user.pro || (!user.pro && registeredTodos < 10))
+  {
+    return next();
+  }
+
+  return response.status(403).json();  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user)
+  {
+    return response.status(404).json({  error: "User does not exists" });
+  }
+
+  const idIsValid = validate(id);
+
+  if (!idIsValid)
+  {
+    return response.status(400).json({ error: "invalid id" });
+  }
+
+  const UserTodo = user.todos.find(todo => todo.id === id);
+
+  if (!UserTodo)
+  {
+    return response.status(404).json({ error: "This todo id does not belong to this username" });
+  }
+
+  request.todo = UserTodo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user)
+  {
+    return response.status(404).json({  error: "User does not exists" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
